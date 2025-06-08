bbb1444969c076d97fbd4cf7744b184dccc33430
Ong Kien Hooi <ongkhooi0711@gmail.com>
2025-06-09 00:27:19 +0800

    Lesson: Iteration

---
+++ iteration/repeat_test.go
@@ -0,0 +1,36 @@
+package iteration
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+)
+
+func Repeat(character string, repeatCount int) string {
+	var repeated strings.Builder
+	for i := 0; i < repeatCount; i++ {
+		repeated.WriteString(character)
+	}
+	return repeated.String()
+}
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a", 5)
+	expected := "aaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("b", 3)
+	fmt.Println(repeated)
+	// Output: bbb
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
